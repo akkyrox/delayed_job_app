c52a2c2a6527727c2c0b6f34a3a61a2271ff7483
Vinay Gupta <vgvinay2@gmail.com>
2015-08-27 14:30:45 +0530

    acanges

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,8 +21,7 @@ class UsersController < ApplicationController
   def edit
   end
 
-  # POST /users
-  # POST /users.json
+  
   def create
     @user = User.new(user_params)
     respond_to do |format|
