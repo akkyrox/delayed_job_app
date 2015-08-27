6ae4603ba76bb41dc4ed05d53fa3738514254b68
Vinay Gupta <vgvinay2@gmail.com>
2015-08-27 15:10:26 +0530

    fixed bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,12 +21,12 @@ class UsersController < ApplicationController
   def edit
   end
 
-  
+
   def create
     @user = User.new(user_params)
     respond_to do |format|
       if @user.save
-        UserMailer.welcome_email(@user).deliver
+        # UserMailer.welcome_email(@user).deliver
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
