c1b176c9d5386679c08f48e7da3a02fdee1786c6
Vinay Gupta <vgvinay2@gmail.com>
2015-08-28 11:04:12 +0530

    remove file

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
     :port                             => 587,
     :domain                       => 'localhost:3000',
     :user_name                 => "vgvinay2@gmail.com",
-    :password                   => "smnetserv123",
+    :password                   => "XXXXXX",
     :authentication           => "plain",
     :enable_starttls_auto => true
 }
